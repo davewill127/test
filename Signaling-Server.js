2e517bdcf38bfd7706a3f2590e5806b550e3f9e6
Siry <git@avspeed.com>
2018-11-02 16:09:03 -0400

    fix AddedVideo event

--- Signaling-Server.js
+++ Signaling-Server.js
@@ -121,7 +121,7 @@ module.exports = exports = function(app, socketCallback) {
 
         socket.on('AddedVideo', function() {
             console.log('added video ' + socket.id + ' user ' + socket.username);
-            socket.broadcast.to(meetingID).emit('onAddedVideo', meetingID, socket.id, socket.username, socket.session); 
+            socket.broadcast.to(users[socket.id].meetingID).emit('onAddedVideo', meetingID, socket.id, socket.username, socket.session); 
         });
 
         socket.on('ScreenSharing', function() {
